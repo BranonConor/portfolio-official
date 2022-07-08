2fc85b22e96517c381a04d4e620ce26dfa22f9f8
branonconor <branoneusebio@gmail.com>
2022-07-07 20:50:14 -0700

    Added blog page with coming soon message

--- src/components/navs/menu.js
+++ src/components/navs/menu.js
@@ -1,18 +1,60 @@
-import './menu.css'
+import "./menu.css";
 
-import ChatIcon from '../icons/chat'
-import HomeIcon from '../icons/home'
-import Link from 'gatsby-link'
-import ProjectsIcon from '../icons/projects'
-import React from "react"
-import UserIcon from '../icons/user'
+import ChatIcon from "../icons/chat";
+import HomeIcon from "../icons/home";
+import Link from "gatsby-link";
+import ProjectsIcon from "../icons/projects";
+import React from "react";
+import UserIcon from "../icons/user";
+import BlogIcon from "../icons/blog";
 
-const Menu  = () => (
-  <nav className='Menu'>
-    <Link to='/' activeStyle={{background: 'var(--grey-bg)', boxShadow: 'inset var(--nav-shadow)'}}><HomeIcon /></Link>
-    <Link to='/about' activeStyle={{background: 'var(--grey-bg)', boxShadow: 'inset var(--nav-shadow)'}}><UserIcon /></Link>
-    <Link to='/projects' activeStyle={{background: 'var(--grey-bg)', boxShadow: 'inset var(--nav-shadow)'}}><ProjectsIcon /></Link>
-    <Link to='/contact' activeStyle={{background: 'var(--grey-bg)', boxShadow: 'inset var(--nav-shadow)'}}><ChatIcon /></Link>
+const Menu = () => (
+  <nav className="Menu">
+    <Link
+      to="/"
+      activeStyle={{
+        background: "var(--grey-bg)",
+        boxShadow: "inset var(--nav-shadow)",
+      }}
+    >
+      <HomeIcon />
+    </Link>
+    <Link
+      to="/about"
+      activeStyle={{
+        background: "var(--grey-bg)",
+        boxShadow: "inset var(--nav-shadow)",
+      }}
+    >
+      <UserIcon />
+    </Link>
+    <Link
+      to="/projects"
+      activeStyle={{
+        background: "var(--grey-bg)",
+        boxShadow: "inset var(--nav-shadow)",
+      }}
+    >
+      <ProjectsIcon />
+    </Link>{" "}
+    <Link
+      to="/blog"
+      activeStyle={{
+        background: "var(--grey-bg)",
+        boxShadow: "inset var(--nav-shadow)",
+      }}
+    >
+      <BlogIcon />
+    </Link>
+    <Link
+      to="/contact"
+      activeStyle={{
+        background: "var(--grey-bg)",
+        boxShadow: "inset var(--nav-shadow)",
+      }}
+    >
+      <ChatIcon />
+    </Link>
   </nav>
-)
-export default Menu
+);
+export default Menu;
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,45 @@
-import React from "react";
+import "aos/dist/aos.css";
+import "./projects.css";
 
-const Blog = () => {
-  return <div>Coming soon!</div>;
+import React, { useEffect } from "react";
+
+import AOS from "aos";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+import { motion } from "framer-motion";
+
+const BlogPage = () => {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  });
+
+  return (
+    <Layout>
+      <SEO title="Blog" />
+      <section className="page BlogPage">
+        <div className="two-column">
+          <div className="title">
+            <motion.h1
+              initial={{ x: -75, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", duration: 0.6 }}
+            >
+              Blog
+            </motion.h1>
+            <motion.h4
+              className="subtitle"
+              initial={{ x: -75, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ type: "spring", duration: 0.8 }}
+            >
+              Blog coming soon! 👀
+            </motion.h4>
+          </div>
+        </div>
+      </section>
+    </Layout>
+  );
 };
 
-export default Blog;
+export default BlogPage;
