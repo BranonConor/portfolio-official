655af1e9dbb4314125407886fa0631ab66fc16e0
branonconor <branoneusebio@gmail.com>
2022-07-10 22:53:45 -0700

    Added some styles to basic blog post cards

--- src/components/grids/blog-grid.js
+++ src/components/grids/blog-grid.js
@@ -1,35 +1,27 @@
 import styled from "styled-components";
 import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
+import { useAllBlogPosts } from "../../hooks/useAllBlogPosts";
+import { motion } from "framer-motion";
 
 const BlogGrid = () => {
-  const data = useStaticQuery(graphql`
-    query BlogPosts {
-      allContentfulBlogPost {
-        edges {
-          node {
-            id
-            slug
-            title
-            description {
-              description
-            }
-            publishDate(formatString: "MMMM YYYY")
-          }
-        }
-      }
-    }
-  `);
+  const posts = useAllBlogPosts();
 
   return (
     <StyledContainer>
-      {data.allContentfulBlogPost.edges.map((node) => {
-        const blogPost = node.node;
+      {posts.map((node, index) => {
+        const { node: post } = node;
         return (
-          <div>
-            <h3>{blogPost.title}</h3>
-            <p>{blogPost.description.description}</p>
-          </div>
+          <StyledCardWrapper
+            initial={{ x: -75, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{
+              type: "spring",
+              duration: Number(`0.${5 + index * 2}`),
+            }}
+          >
+            <StyledCardTitle>{post.title}</StyledCardTitle>
+            <p>{post.description.description}</p>
+          </StyledCardWrapper>
         );
       })}
     </StyledContainer>
@@ -40,5 +32,33 @@ export default BlogGrid;
 
 const StyledContainer = styled.div`
   width: 100%;
-  padding: 32px;
+  padding: 16px 0;
+`;
+
+const StyledCardWrapper = styled(motion.div)`
+  width: 100%;
+  background: #111111;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 10px;
+
+  &:hover {
+    background: linear-gradient(124.41deg, #0072b1 0%, #fd36ab 95.2%);
+    cursor: pointer;
+
+    h5 {
+      color: white;
+      border-left: 2px solid white;
+    }
+  }
+`;
+
+const StyledCardTitle = styled.h5`
+  font-family: "Comfortaa", sans-serif;
+  font-weight: bold;
+  width: auto;
+  color: #189ee6;
+  border-left: 2px solid #189ee6;
+  padding: 0px 0px 0px 16px;
+  margin: 8px 0px 4px 0;
 `;
--- src/hooks/useAllBlogPosts.js
+++ src/hooks/useAllBlogPosts.js
@@ -3,7 +3,7 @@ import { useStaticQuery, graphql } from "gatsby";
 export const useAllBlogPosts = () => {
   const data = useStaticQuery(graphql`
     query BlogPosts {
-      allContentfulBlogPost {
+      allContentfulBlogPost(limit: 10) {
         edges {
           node {
             id
